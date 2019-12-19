226767f0c2dc36ec07cdad1ff86258974123a6eb
Gustavo Maronato <gustavomaronato@gmail.com>
2019-12-18 21:08:09 -0300

    Auto generate docs with Travis (#26)
    * Auto generate docs with Travis
    
    * use node ^12
    
    * added test command

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,18 @@ module.exports = {
 
   overrides: [
     {
-      files: ["**/__tests__/*.{j,t}s?(x)"],
+      files: [
+        '**/__tests__/*.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    },
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
       env: {
         jest: true
       }
