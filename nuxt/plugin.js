780683d6d788d14ff47094af39871dba4ec5b1b9
Maronato <gustavomaronato@gmail.com>
2020-03-19 13:43:27 -0300

    removed nuxt plugin ignores

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,9 +1,6 @@
-/* eslint-disable */
-// @ts-nocheck
 import Vue from "vue";
 import Toast from "vue-toastification";
 
-// @ts-ignore
 Vue.use(Toast, <%= serialize(options.pluginOptions) %>)
 
 export default function (ctx, inject) {
