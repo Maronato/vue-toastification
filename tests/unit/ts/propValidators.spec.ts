6de29e57a255b3b0c0b34339029a7614c28fcadd
Linhart Tomáš <linhart@cngroup.dk>
2020-03-24 15:57:34 +0100

    Fix unit test

--- tests/unit/ts/propValidators.spec.ts
+++ tests/unit/ts/propValidators.spec.ts
@@ -1,11 +1,10 @@
 import PROPS from "../../../src/ts/propValidators";
+import { PropOptions } from "vue/types/options";
 
 describe("CONTAINER props", () => {
-  it.skip("default CONTAINER.container is document.body", () => {
-    // TODO: fix typescript error
-    // Property 'default' does not exist on type 'PropValidator<HTMLElement>'.
-    // Property 'default' does not exist on type 'new (...args: string[]) => Function'.
-    //
-    // expect(PROPS.CONTAINER.container.default()).toBe(document.body);
+  it("default CONTAINER.container is document.body", () => {
+    expect((PROPS.CONTAINER.container as PropOptions).default()).toBe(
+      document.body
+    );
   });
 });
