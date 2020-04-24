9fd524d90722e8c278de6722e77bbb2f3b3b3e43
Gustavo Maronato <gustavomaronato@gmail.com>
2020-04-23 22:51:45 -0300

    Fix nuxt options. Resolves #69 (#71)

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Toast from "vue-toastification";
 
-Vue.use(Toast, <%= serialize(options.pluginOptions) %>)
+Vue.use(Toast, <%= serialize(options) %>)
 
 export default function (ctx, inject) {
   inject('toast', Vue.$toast)
