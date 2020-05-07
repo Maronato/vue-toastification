24df3187a6f57d7f28e9e3f10c8191324eadfe96
Maronato <gustavomaronato@gmail.com>
2020-05-07 19:49:43 -0300

    Fix containerClassName not being updated by updateDefaults

--- src/components/VtToastContainer.vue
+++ src/components/VtToastContainer.vue
@@ -4,7 +4,7 @@
       <Transition
         :transition="defaults.transition"
         :transition-duration="defaults.transitionDuration"
-        :class="getClasses(pos)"
+        :class="getClasses(pos).concat(defaults.containerClassName)"
       >
         <Toast
           v-for="toast in getPositionToasts(pos)"
@@ -149,7 +149,7 @@ export default Vue.extend({
     },
     getClasses(position: POSITION) {
       const classes = [`${VT_NAMESPACE}__container`, position];
-      return classes.concat(this.containerClassName);
+      return classes;
     }
   }
 });
