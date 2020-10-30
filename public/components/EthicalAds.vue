2936f3de0a47cd5cc01832830c5fb21391fad04c
Maronato <gustavomaronato@gmail.com>
2020-10-30 17:57:05 -0300

    more ad placements

---
+++ public/components/EthicalAds.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    :id="adId"
+    :data-ea-type="type"
+    data-ea-publisher="maronato-github-iovue-toastification"
+    data-ea-keywords="vue|frontend|design|javascript|typescript"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    adId: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+  },
+};
+</script>
