49e0f7868ffa63428b7c1a0ee019dc073991c02b
Gustavo Maronato <gustavo@maronato.dev>
2022-01-30 01:20:43 -0300

    VtTransition uses script setup

--- src/components/VtTransition.vue
+++ src/components/VtTransition.vue
@@ -13,31 +13,36 @@
     <slot></slot>
   </transition-group>
 </template>
-<script lang="ts">
-// Transition methods taken from https://github.com/BinarCode/vue2-transitions
-import { defineComponent } from "vue"
-import { getProp } from "../ts/utils"
-import PROPS from "../ts/propValidators"
 
+<script lang="ts">
+import { defineComponent } from "vue"
+import { PluginOptions } from ".."
+import { PLUGIN_DEFAULTS } from "../ts/propValidators"
 export default defineComponent({
   name: "VtTransition",
-  props: PROPS.TRANSITION,
-
-  emits: ["leave"],
-
-  setup() {
-    const leave = (el: unknown) => {
-      if (el instanceof HTMLElement) {
-        el.style.left = el.offsetLeft + "px"
-        el.style.top = el.offsetTop + "px"
-        el.style.width = getComputedStyle(el).width
-        el.style.position = "absolute"
-      }
-    }
-    return {
-      getProp,
-      leave,
-    }
-  },
 })
+</script>
+
+<script lang="ts" setup>
+// Transition methods taken from https://github.com/BinarCode/vue2-transitions
+import { getProp } from "../ts/utils"
+
+interface TransitionProps {
+  transition?: PluginOptions["transition"]
+}
+
+defineEmits(["leave"])
+
+withDefaults(defineProps<TransitionProps>(), {
+  transition: PLUGIN_DEFAULTS.transition,
+})
+
+const leave = (el: unknown) => {
+  if (el instanceof HTMLElement) {
+    el.style.left = el.offsetLeft + "px"
+    el.style.top = el.offsetTop + "px"
+    el.style.width = getComputedStyle(el).width
+    el.style.position = "absolute"
+  }
+}
 </script>
