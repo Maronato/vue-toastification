c928e11c59ec3ac3ab70437efed35317f178fc54
Gustavo Maronato <gustavo@maronato.dev>
2022-01-30 01:12:27 -0300

    upgrade dependencies

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -1,22 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/*
- ** TailwindCSS Configuration File
- **
- ** Docs: https://tailwindcss.com/docs/configuration
- ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
- */
-
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: [],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === "production",
-    content: ["./src/**/*.vue", "./src/**/*.ts", "./src/**/*.tsx", "./index.html"],
-  },
+  content: ["./src/**/*.{html,ts,tsx,vue}"],
 }
