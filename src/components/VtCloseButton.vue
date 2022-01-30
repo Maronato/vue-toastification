fd27c2c934fe66d18c4c6c89ca21239051ef0f67
Gustavo Maronato <gustavo@maronato.dev>
2022-01-30 01:14:31 -0300

    VtCloseButton uses script setup

--- src/components/VtCloseButton.vue
+++ src/components/VtCloseButton.vue
@@ -8,36 +8,48 @@
     &times;
   </component>
 </template>
-<script lang="ts">
-import { computed, defineComponent } from "vue"
 
-import { VT_NAMESPACE } from "../ts/constants"
-import PROPS from "../ts/propValidators"
-import { getVueComponentFromObj } from "../ts/utils"
+<script lang="ts">
+import { defineComponent, computed } from "vue"
 
 export default defineComponent({
   name: "VtCloseButton",
+})
+</script>
 
-  props: PROPS.CLOSE_BUTTON,
+<script lang="ts" setup>
+import { PluginOptions } from ".."
 
-  setup(props) {
-    const buttonComponent = computed(() => {
-      if (props.component !== false) {
-        return getVueComponentFromObj(props.component)
-      }
-      return "button"
-    })
-    const classes = computed(() => {
-      const classes = [`${VT_NAMESPACE}__close-button`]
-      if (props.showOnHover) {
-        classes.push("show-on-hover")
-      }
-      return classes.concat(props.classNames)
-    })
-    return {
-      buttonComponent,
-      classes,
-    }
-  },
+import { VT_NAMESPACE } from "../ts/constants"
+import { PLUGIN_DEFAULTS } from "../ts/propValidators"
+import { getVueComponentFromObj } from "../ts/utils"
+import { ClassNames } from "../types"
+
+interface CloseButtonProps {
+  component?: NonNullable<PluginOptions["closeButton"]>
+  classNames?: ClassNames
+  showOnHover?: boolean
+  ariaLabel?: string
+}
+
+const props = withDefaults(defineProps<CloseButtonProps>(), {
+  component: PLUGIN_DEFAULTS.closeButton,
+  classNames: PLUGIN_DEFAULTS.closeButtonClassName,
+  ariaLabel: PLUGIN_DEFAULTS.accessibility()["closeButtonLabel"],
+  showOnHover: PLUGIN_DEFAULTS.showCloseButtonOnHover,
+})
+
+const buttonComponent = computed(() => {
+  if (props.component !== false) {
+    return getVueComponentFromObj(props.component)
+  }
+  return "button"
+})
+const classes = computed(() => {
+  const classes = [`${VT_NAMESPACE}__close-button`]
+  if (props.showOnHover) {
+    classes.push("show-on-hover")
+  }
+  return classes.concat(props.classNames)
 })
 </script>
