6e0de5481a830a00ad181438f560d26ffb8e177a
Gustavo Maronato <gustavo@maronato.dev>
2022-01-30 15:18:38 -0300

    remove unused prop validators

--- tests/unit/ts/propValidators.spec.ts
+++
@@ -1,9 +0,0 @@
-import PROPS from "../../../src/ts/propValidators"
-
-describe("CONTAINER props", () => {
-  it("default CONTAINER.container is document.body", () => {
-    expect(
-      (PROPS.CONTAINER.container as { default: () => void }).default()
-    ).toBe(document.body)
-  })
-})
