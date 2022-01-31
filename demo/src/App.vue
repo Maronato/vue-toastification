2a371e6ade3d0d1c5e07a7581886afb1229e7013
Gustavo Maronato <gustavo@maronato.dev>
2022-01-30 22:33:32 -0300

    display random toasts in demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <img alt="Vue logo" src="./assets/logo.png" /><br />
+  <button @click="showToast">Show toast</button>
 </template>
 
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/HelloWorld.vue"
+import { POSITION, TYPE, useToast } from "../../src"
+
+const toast = useToast()
+
+const getRandom = <T>(list: T[]) =>
+  list[Math.floor((Math.random() * 10) % list.length)]
+
+const randomType = () => getRandom(Object.values(TYPE))
+
+const randomPosition = () => getRandom(Object.values(POSITION))
+
+const randomMessage = () =>
+  getRandom(["Hello", "I'm a toast!", "Oops!", "Hi :)"])
+
+const showToast = () =>
+  toast(randomMessage(), { type: randomType(), position: randomPosition() })
 </script>
 
 <style>
